dfd3c31c2b9103f036eb5e4eec5417b582b0790f
KalaburigiPoojitha <kalaburigipoojitha05@gmail.com>
2024-06-24 14:21:05 +0530

    Add files via upload

---
+++ react2/src/App.js
@@ -0,0 +1,203 @@
+// import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>The CEO Magazine</h1>
+      <div className="nav">
+        <div className="navBar">Home</div>
+        <div className="navBar">Women</div>
+        <div className="navBar">Magazine</div>
+        <div className="navBar">StartUps</div>
+        <div className="navBar">Technologies</div>
+        <div className="navBar">Success Stories</div>
+        <div className="navBar">Blog</div>
+        <div className="navBar">Nominate Now</div>
+        <div className="navBar">More</div>
+        <div className="navBar">Subscribe Now</div>
+      </div>
+      <h1>Top 10 Business Magazines in India 2024</h1>
+      <div>
+        <p>
+          The collaboration of inspiring business leaders' guidance, success
+          stories of the brands, and exploration of various aspects of business,
+          all designed by visionaries with professional and appealing designs,
+          make the brands featured in editions stand out—an ultimate goal, isn't
+          it?
+        </p>
+        <p>
+          What makes business magazines in India stand out is the quality of
+          their editorial content and design, coupled with advertising.
+        </p>
+        <p>
+          The regular editions of the <ins>best business magazines</ins> feature
+          various industries such as Finance, Tax, Law, Accountancy, Technology,
+          BPO and BPM, Property, People, etc., catering to their readership.
+          Below are the top 10 business magazines in India listed alphabetically
+        </p>
+      </div>
+      <div className="mainConatiner">
+        <div className="subContainer class1">
+          <h3>Business India</h3>
+          <img src="./images/Business-India-logo.webp" alt=""></img>
+          <p>Categories: Business News</p>
+          <p>Frequency: Fortnightly</p>
+          <p>Publisher: Ashok Hotchand Advani</p>
+          <p>Founded: 1978</p>
+          <p>First issue: February 1978</p>
+          <p>Company: Business India Publications Ltd.</p>
+          <p>Country: India</p>
+          <p>Based in: Mumbai</p>
+          <p>Language: English</p>
+          <p>Website: Business India</p>
+        </div>
+
+        <div className="subContainer class1">
+          <h3>Business Today (India)</h3>
+          <img src="./images/btlogo.png" alt=""></img>
+          <p>Categories: Business magazine</p>
+          <p>Frequency: Fortnightly</p>
+          <p>Publisher: Living Media</p>
+          <p>Founded: 1992</p>
+          <p>First issue: February 1978</p>
+          <p>Company: Business Today Publications Ltd.</p>
+          <p>Country: India</p>
+          <p>Based in: Mumbai</p>
+          <p>Language: English</p>
+          <p>Website: Business Today</p>
+        </div>
+
+        <div className="subContainer class1">
+          <h3>Entrepreneur</h3>
+          <img src="./images/emlogo.png" alt=""></img>
+          <p>Editor-in-Chief: Jason Feifer</p>
+          <p>Frequency: Monthly</p>
+          <p>Total Circulation:510483</p>
+          <p>Founded: 1977</p>
+          <p>First issue: February 1978</p>
+          <p>Company: Entrepreneur Media, Inc.</p>
+          <p>Country: United States</p>
+          <p>Based in: Irvine,California,U.S.</p>
+          <p>Language: English</p>
+          <p>Website: entrepreneur.com</p>
+        </div>
+
+        <div className="subContainer class2">
+          <h3>Forbes</h3>
+          <img src="./images/forbeslogo.png" alt=""></img>
+          <p>Categories:Business Magazine</p>
+          <p>Editor:Randall Lane </p>
+          <p>Frequency: Twice quarterly</p>
+          <p>Founder: B.C.Forbes</p>
+          <p>First issue:Septeber 15, 1917;106 years ago</p>
+          <p>Company: Integrated Whale Media Investments</p>
+          <p>Country: United States</p>
+          <p>Based in: Jersey City,New Jersey, U.S.</p>
+          <p>Language: English</p>
+          <p>Website: entrepreneur.com</p>
+        </div>
+
+        <div className="subContainer class2">
+          <h3>Fortune</h3>
+          <img src="./images/fortunelogo.png" alt=""></img>
+          <p>Categories:Business Magazine</p>
+          <p>Editor: Alyson Shontell</p>
+          <p>Frequency: Monthly</p>
+          <p>Founder: Henry Luce</p>
+          <p>First issue: 1929; 95 years ago</p>
+          <p>Publisher: Fortune Media Group</p>
+          <p>Country: United States</p>
+          <p>Based in: New York City, U.S.</p>
+          <p>Language: English</p>
+          <p>Website: fortune.com</p>
+        </div>
+
+        <div className="subContainer class2">
+          <h3>Inc. India</h3>
+          <img src="./images/inclogo.png" alt=""></img>
+          <p>Categories:Business </p>
+          <p>Editor:Anuradha Das Mathur</p>
+          <p>Frequency: Monthly</p>
+          <p>Company: 9.9 Media</p>
+          <p>First issue: February 2009</p>
+          <p>Publisher: Fortune Media Group</p>
+          <p>Country: United States</p>
+          <p>Based in: New York City, U.S.</p>
+          <p>Language: English</p>
+          <p>Website: fortune.com</p>
+        </div>
+
+        <div className="subContainer class3">
+          <h3>Outlook Business Magazine</h3>
+          <img src="./images/outlooklogo.jpeg" alt=""></img>
+          <p>Categories: News Magazine </p>
+          <p>Editor: Chinki Sinha</p>
+          <p>Former: Rajesh Ramchandran</p>
+          <p>Company: Rajan Rahej Group</p>
+          <p>First issue: October 1995</p>
+          <p>Circulation: 4,25,000</p>
+          <p>Country: India</p>
+          <p>Based in: New Delhi</p>
+          <p>Language: English</p>
+          <p>Website: outlookindia.com</p>
+        </div>
+
+        <div className="subContainer class3">
+          <h3>The Economist</h3>
+          <img src="./images/economicstlogo.jpeg" alt=""></img>
+          <p>Categories: Weekly Newspaper </p>
+          <p>Editor: Zanny Minton Beddoes</p>
+          <p>Founder: James Wilson</p>
+          <p>Owner: The Economist Group</p>
+          <p>Founded: September 1843</p>
+          <p>Political: Radical centrism</p>
+          <p>Country: England</p>
+          <p>Based in: London</p>
+          <p>Language: English</p>
+          <p>Website: www.economist.com</p>
+        </div>
+
+        <div className="subContainer class3">
+          <h3>Time Magazine</h3>
+          <img src="./images/timeslogo.png" alt=""></img>
+          <p>Categories: News Magazine </p>
+          <p>Editor: Sam Jacobs</p>
+          <p>Frequency: Weekly, twice monthly on fridays</p>
+          <p>Company: Times Inc,Meredith Corporation</p>
+          <p>First issue: March 3,1923</p>
+          <p>Political: Radical centrism</p>
+          <p>Country: United States</p>
+          <p>Based in: New York</p>
+          <p>Language: English</p>
+          <p>Website: time.com</p>
+        </div>
+      </div>
+      <div className="footContainer">
+        <div>Contact</div>
+        <div>6605 Cypresswood Dr. Suite 425 Houston TX 77379 USA</div>
+        <div>
+          Voice+1 (832) 717-4445 Fax+1 (832) 717-4460 Email: info@codemag.com
+          1993 - 2024 EPS Software Corp.
+        </div>
+        <div>
+          <a href="www.privacypolicy.com" target="_blank">
+            Privacy Policy
+          </a>
+        </div>
+        <div>
+          Site Map Consulting Consulting Home Services & Technologies Artificial
+          Intelligence (AI) Cloud Computing Custom Application Development
+          Executive Briefing (AI) Low-Code/No-Code Cyber Security Copilots in
+          Your Apps! Project Rescue CODE Labs Business Document Copilot Legacy
+          Conversion and Maintenance Free Hour of Consulting VFP Conversion
+          Energy Software Contact Us Magazine Magazine Home CODE Focus Magazine
+          All Issues Subscribe Mobile My (Digital) Magazines Where is my
+          Magazine? My Subscriber Account Advertise Write
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
